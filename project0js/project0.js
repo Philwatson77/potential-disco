e01d64260cf3f82204ec63836b2d2de135de22d6
Phillip Watson <phillwatson@Phillips-MacBook-Pro.local>
2022-03-25 09:37:41 +1100

    colorupdate

--- project0js/project0.js
+++ project0js/project0.js
@@ -9,7 +9,7 @@ if (playerX && repete == 1) {
     currentGame[$ ('.cell').index(this)] = 'x'
     playerX = !playerX 
 } else if (repete == 1) {
-    $ (this).addClass('circle ')
+    $ (this).addClass('circle')
     currentGame[$ ('.cell').index(this)] = 'o'
     playerX = !playerX 
 }
@@ -17,25 +17,26 @@ checkForWin()
 }
 const checkForWin = function() {
     if (currentGame[0] == currentGame[1] && currentGame[0] == currentGame[2] && currentGame[0] != '') {
-        console.log(currentGame[0] + ' Wins')
+        alert(currentGame[0] + ' Wins')
     } else if (currentGame[3] == currentGame[4] && currentGame[3] == currentGame[5] && currentGame[3] != '') {
-        console.log(currentGame[3] + ' Wins')
+        alert(currentGame[3] + ' Wins')
     } else if (currentGame[6] == currentGame[7] && currentGame[6] == currentGame[8] && currentGame[6] != '') {
-        console.log(currentGame[6] + ' Wins')
+        alert(currentGame[6] + ' Wins')
     } else if (currentGame[0] == currentGame[3] && currentGame[0] == currentGame[6] && currentGame[0] != '') {
-        console.log(currentGame[0] + ' Wins')
+        alert(currentGame[0] + ' Wins')
     } else if (currentGame[1] == currentGame[4] && currentGame[1] == currentGame[7] && currentGame[1] != '') {
-        console.log(currentGame[1] + ' Wins')
+        alert(currentGame[1] + ' Wins')
     } else if (currentGame[2] == currentGame[5] && currentGame[2] == currentGame[8] && currentGame[2] != '') {
-        console.log(currentGame[2] + ' Wins')
+        alert(currentGame[2] + ' Wins')
     } else if (currentGame[0] == currentGame[4] && currentGame[0] == currentGame[8] && currentGame[0] != '') {
-        console.log(currentGame[0] + ' Wins')
+        alert(currentGame[0] + ' Wins')
     } else if (currentGame[2] == currentGame[4] && currentGame[2] == currentGame[6] && currentGame[2] != '') {
-        console.log(currentGame[2] + ' Wins')
+        alert(currentGame[2] + ' Wins')
 
     } 
     if (!currentGame.includes('')) { 
-        console.log('draw')
+        alert('draw')
+    
     }  
     }; 
     
@@ -44,7 +45,7 @@ const reset = function() {
     currentGame[i] = ''
     $ ('.cell').eq(i).removeClass('x circle')
     } 
-    playerX = true 
+    playerX = true
 }  
 $ ('.cell').on('click', playerTurn)
 $ ('#restartButton').on('click', reset)
